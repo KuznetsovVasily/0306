c503b2417b4b01275b52353cc779b66ac412cb64
Vasily.Kuznetsov <psstarcraft2@gmail.com>
2017-06-03 18:13:50 +0300

    Dictionary start

---
+++ 0106/Vasily.Kuznetsov/NatureSimulationGen2Sol/NatureSimulationGen2/Global/EntityFactory.cs
@@ -0,0 +1,21 @@
+﻿using NatureSimulationGen2.Animal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NatureSimulationGen2.Global
+{
+    public class EntityFactory
+    {
+        public Dictionary<int, Func<Tuple<int, int>, Entity>> RandomEntity = new Dictionary<int, Func<Tuple<int, int>, Entity>>
+        {
+            {1, new Owl(RandomHolder.GetInstance().GetRandomX, RandomHolder.GetInstance().GetRandomY },
+            
+        };
+
+        
+
+    }
+}
